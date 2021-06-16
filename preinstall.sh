cb624e4e759b90b308a7e4fdffff414c4b0fb898
vamshi-beeram <77605877+vamshi-beeram@users.noreply.github.com>
2021-06-16 23:31:42 +0530

    installing for dual boot

--- preinstall.sh
+++ preinstall.sh
@@ -8,90 +8,75 @@
 #-------------------------------------------------------------------------
 
 echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
+echo "Setting timedatectl"
 echo "-------------------------------------------------"
 timedatectl set-ntp true
-pacman -S --noconfirm pacman-contrib
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
-
-
-
-echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm gptfdisk btrfs-progs
 
 echo "-------------------------------------------------"
 echo "-------select your disk to format----------------"
 echo "-------------------------------------------------"
-lsblk
+fdisk -l
 echo "Please enter disk: (example /dev/sda)"
 read DISK
 echo "--------------------------------------"
-echo -e "\nFormatting disk...\n$HR"
+echo -e "\nPartitioning disk...\n"
 echo "--------------------------------------"
 
-# disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+cfdisk ${DISK}
 
-# create partitions
-sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+fdisk -l
+echo "please enter boot partition"
+read BPART
+echo "\n please enter swap partition"
+read SPART
+echo "\n please enter root partition"
+read RPART
 
-# set partition types
-sgdisk -t 1:ef00 ${DISK}
-sgdisk -t 2:8300 ${DISK}
+echo "Formatting partitions"
+mkfs.fat -F32 ${BPART}
+mkfs.ext4 ${RPART}
+mkswap ${SPART}
+swapon ${SPART}
 
-# label partitions
-sgdisk -c 1:"UEFISYS" ${DISK}
-sgdisk -c 2:"ROOT" ${DISK}
-
-# make filesystems
-echo -e "\nCreating Filesystems...\n$HR"
-
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
-mkfs.ext4 -L "ROOT" "${DISK}2"
-
-# mount target
-mkdir /mnt
-mount -t ext4 "${DISK}2" /mnt
+echo "--------------------------------------"
+echo "-- Mounting Partitions       --"
+echo "--------------------------------------"
+mount ${RPART} /mnt
 mkdir /mnt/boot
-mkdir /mnt/boot/efi
-mount -t vfat "${DISK}1" /mnt/boot/
+mount ${BPART} /mnt/boot
 
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
-pacstrap /mnt base base-devel linux linux-firmware vim nano sudo --noconfirm --needed
+pacstrap /mnt base base-devel linux-lts linux-firmware util-linux grub efibootmgr os-prober vim networkmanager nano sudo --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
-echo "--------------------------------------"
-echo "-- Bootloader Systemd Installation  --"
-echo "--------------------------------------"
-bootctl install
-cat <<EOF > /boot/loader/entries/arch.conf
-title Arch Linux  
-linux /vmlinuz-linux  
-initrd  /initramfs-linux.img  
-options root=${DISK}1 rw
-EOF
-
-echo "--------------------------------------"
-echo "--          Network Setup           --"
-echo "--------------------------------------"
-pacman -S networkmanager dhclient --noconfirm --needed
-systemctl enable --now NetworkManager
+echo "Setting up language, locale and clock"
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+nano /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo vneeha > /etc/hostname
+echo "127.0.0.1 localhost
+::1   localhost
+127.0.0.1   vneeha.localdomain vneeha" >> /etc/hosts
 
 echo "--------------------------------------"
 echo "--      Set Password for Root       --"
 echo "--------------------------------------"
 echo "Enter password for root user: "
-passwd root
+passwd
 
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+systemctl enable NetworkManager
+
+echo "Bootloader"
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 exit
-umount -R /mnt
 
-echo "--------------------------------------"
-echo "--   SYSTEM READY FOR FIRST BOOT    --"
-echo "--------------------------------------"
